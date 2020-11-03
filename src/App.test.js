d1b9c1e7cc7604648a5e26ad9d43b615fb0cd912
Dhara Patel <dhara.patel95@hotmail.co.uk>
2020-11-03 12:02:39 +0000

    Remove duplicated test from App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,3 @@ test('Shows To Do List App on page', () => {
 
   expect(screen.getByText('To Do List App')).toBeInTheDocument()
 })
-
-test('user can view list', () => {
-  render(<ToDoList />);
-
-  expect(screen.getByText('Apply to Jobs')).toBeInTheDocument()
-})
